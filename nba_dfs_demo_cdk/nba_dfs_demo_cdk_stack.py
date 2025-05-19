62313efcdf7e227d53442a8ee1c90c2789b55d32
vincent-j-chang <vincentchang.dev@gmail.com>
2025-05-19 08:40:04 -0400

    update the stack file with the lambda section

--- nba_dfs_demo_cdk/nba_dfs_demo_cdk_stack.py
+++ nba_dfs_demo_cdk/nba_dfs_demo_cdk_stack.py
@@ -4,7 +4,7 @@ from aws_cdk import Stack
 from nba_dfs_demo_cdk.constructs.storage import CsvStorageBucket
 # These will be uncommented when you implement them
 # from nba_dfs_demo_cdk.constructs.email import EmailService
-# from nba_dfs_demo_cdk.constructs.processing import CsvProcessor
+from nba_dfs_demo_cdk.constructs.processing import CsvProcessor
 
 class NbaDfsDemoCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs):
@@ -12,3 +12,10 @@ class NbaDfsDemoCdkStack(Stack):
         
         # Create S3 storage
         self.storage = CsvStorageBucket(self, "CsvStorage")
+
+        # Create Lambda and connect to the S3 bucket
+        self.processor = CsvProcessor(
+            self, 
+            "CsvProcessor"
+            storage_bucket=self.storage_bucket
+        )

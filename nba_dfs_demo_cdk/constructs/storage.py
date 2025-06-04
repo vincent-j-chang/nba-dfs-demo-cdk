74505dd09cc3dad2d7256a944ea2bbeffd2b0de8
vincent-j-chang <vincentchang.dev@gmail.com>
2025-06-04 08:31:21 -0400

    updated storage.py file to remove duplicate rawemailfilesbucket class

--- nba_dfs_demo_cdk/constructs/storage.py
+++ nba_dfs_demo_cdk/constructs/storage.py
@@ -19,19 +19,6 @@ class RawEmailFilesBucket(Construct):
             block_public_access=s3.BlockPublicAccess.BLOCK_ALL,  # Security best practice
         )
 
-class RawEmailFilesBucket(Construct):
-    def __init__(self, scope: Construct, id: str, **kwargs):
-        super().__init__(scope, id)
-
-        # Create S3 bucket to store raw email files from SES
-        self.bucket = s3.Bucket(
-            self, 
-            "RawEmailBucket",
-            bucket_name="nba-dfs-demo-cdk-raw-email",
-            removal_policy=RemovalPolicy.RETAIN,  # Protect against accidental deletion
-            encryption=s3.BucketEncryption.S3_MANAGED,  # Enable encryption
-            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,  # Security best practice
-        )
 
 
 class CsvStorageBucket(Construct):
